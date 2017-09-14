6de9c06477dee2ac8369e2b5eca7fd294a32b297
kumar <manojkun@gmail.com>
2017-09-14 14:41:45 -0500

    Here i jave added the multiplication feature and also complued the code

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 class Test
 {
+	public static void main(String args[])
+	{
+		
 	int a = 10;
 	int b = 20;
-	public static void main(STring args[])
-	{
 		int c = a+b;
 		System.out.println(c);
 		int d = a*b;
